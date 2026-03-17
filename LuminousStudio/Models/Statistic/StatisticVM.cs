0f98f90fb2670d4910aee27fdb549be9beb1808f
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-03-17 03:54:27 +0200

    Add view models for client and statistics features

---
+++ LuminousStudio/Models/Statistic/StatisticVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuminousStudio.Models.Statistic
+{
+    public class StatisticVM
+    {
+        [Display(Name = "Count Clients")]
+        public int CountClients { get; set; }
+
+        [Display(Name = "Count Tiffany Lamps")]
+        public int CountTiffanyLamps { get; set; }
+
+        [Display(Name = "Count Orders")]
+        public int CountOrders { get; set; }
+
+        [Display(Name = "Total Sum Orders")]
+        public decimal SumOrders { get; set; }
+    }
+}
