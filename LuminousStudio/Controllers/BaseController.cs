d293231f434debebfe179b03437c819a801bc801
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-03-31 00:16:23 +0300

    Add BaseController with GetCurrentUserId helper

---
+++ LuminousStudio/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿namespace LuminousStudio.Controllers
+{
+    using System.Security.Claims;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : Controller
+    {
+        protected Guid? GetCurrentUserId()
+        {
+            var userIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (userIdString == null)
+            {
+                return null;
+            }
+
+            return Guid.Parse(userIdString);
+        }
+    }
+}
