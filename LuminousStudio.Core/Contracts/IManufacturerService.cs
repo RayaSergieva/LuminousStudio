edd16a4bea0d24fd2c6799af016cdc52decb2a80
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-04-02 23:59:45 +0300

    Refactor IManufacturerService with async contracts and improve code readability

--- LuminousStudio.Core/Contracts/IManufacturerService.cs
+++ LuminousStudio.Core/Contracts/IManufacturerService.cs
@@ -1,11 +1,11 @@
-﻿using LuminousStudio.Infrastructure.Data.Entities;
-
-namespace LuminousStudio.Core.Contracts
+﻿namespace LuminousStudio.Core.Contracts
 {
+    using LuminousStudio.Infrastructure.Data.Entities;
+
     public interface IManufacturerService
     {
-        List<Manufacturer> GetManufacturers();
-        Manufacturer GetManufacturerById(int manufacturerId);
-        List<TiffanyLamp> GetProductsByManufacturer(int manufacturerId);
+        Task<List<Manufacturer>> GetManufacturersAsync();
+        Task<Manufacturer?> GetManufacturerByIdAsync(Guid manufacturerId);
+        Task<List<TiffanyLamp>> GetProductsByManufacturerAsync(Guid manufacturerId);
     }
 }
