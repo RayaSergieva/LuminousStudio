4afcc7a381dc8faaa9766be29c57e7f53b806f16
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-04-21 15:43:03 +0300

    Update Program.cs with clean extension methods and middleware pipeline

--- LuminousStudio/Program.cs
+++ LuminousStudio/Program.cs
@@ -1,64 +1,50 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-using LuminousStudio.Core.Contracts;
-using LuminousStudio.Core.Services;
-using LuminousStudio.Infrastructure.Data;
-using LuminousStudio.Infrastructure.Data.Entities;
-using LuminousStudio.Infrastructure.Data.Infrastructure;
+using LuminousStudio.Data.Repository;
+using LuminousStudio.Services.Common;
+using LuminousStudio.Services.Core.Services;
+using LuminousStudio.Web.Hubs;
+using LuminousStudio.Web.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = false;
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequiredLength = 5;
-})
-    .AddRoles<IdentityRole<Guid>>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 builder.Services.AddControllersWithViews();
-
-builder.Services.AddTransient<ILampStyleService, LampStyleService>();
-builder.Services.AddTransient<IManufacturerService, ManufacturerService>();
-builder.Services.AddTransient<ITiffanyLampService, TiffanyLampService>();
-builder.Services.AddTransient<IOrderService, OrderService>();
-builder.Services.AddTransient<IStatisticService, StatisticService>();
-builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+builder.Services.AddSignalR();
+builder.Services.AddScoped<IStockHubService, StockHubService>();
+builder.Services.AddRepositories(typeof(TiffanyLampRepository).Assembly);
+builder.Services.AddUserDefinedServices(typeof(TiffanyLampService).Assembly);
+builder.Services.AddSeeders();
+builder.Services.AddAdminServices();
 
 var app = builder.Build();
 
+app.SeedDefaultIdentity();
 await app.PrepareDatabase();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseGlobalExceptionHandling();
+    app.UseExceptionHandler("/Home/Error?statusCode=500");
     app.UseHsts();
 }
 
+app.UseSecurityHeaders();
+app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
+app.UseAdminRedirection();
+app.MapHub<StockHub>("/stockHub");
 app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
