d0e64820daa39e089600457b4b4235b5bd091c85
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-04-21 15:27:49 +0300

    Add strongly-typed configuration classes for Admin and Identity settings

---
+++ LuminousStudio.Web.Common/Configuration/AdminSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace LuminousStudio.Web.Common.Configuration
+{
+    public class AdminSettings
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ LuminousStudio.Web.Common/Configuration/IdentitySettings.cs
@@ -0,0 +1,12 @@
+﻿namespace LuminousStudio.Web.Common.Configuration
+{
+    public class IdentitySettings
+    {
+        public bool RequireConfirmedAccount { get; set; }
+        public bool RequireDigit { get; set; }
+        public bool RequireLowercase { get; set; }
+        public bool RequireUppercase { get; set; }
+        public bool RequireNonAlphanumeric { get; set; }
+        public int RequiredLength { get; set; }
+    }
+}
