efb8a9fedf7a588870df4b21ed3401677484d611
Raya Sergieva <r.sergieva2020@pmg-pernik.com>
2026-04-21 15:52:34 +0300

    Add ApplicationDbContext with Fluent API configuration support via ApplyConfigurationsFromAssembly

---
+++ LuminousStudio.Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿namespace LuminousStudio.Data
+{
+    using System.Reflection;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    using LuminousStudio.Data.Models;
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public virtual DbSet<LampStyle> LampStyles { get; set; }
+
+        public virtual DbSet<Order> Orders { get; set; }
+
+        public virtual DbSet<TiffanyLamp> TiffanyLamps { get; set; }
+
+        public virtual DbSet<ShoppingCart> ShoppingCarts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
